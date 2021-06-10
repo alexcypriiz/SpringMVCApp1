818c20052f439c44baef5668d6221301b5047849
Alexey <80638551+alexcypriiz@users.noreply.github.com>
2021-06-11 01:28:50 +0400

    Revert "Refactor/use springmvc model (#5)"
    This reverts commit ca750c004b02ca532a3c4f03155414b62826dc65.

--- src/main/java/ru/podlubnyy/springcourse/controllers/FirstController.java
+++ src/main/java/ru/podlubnyy/springcourse/controllers/FirstController.java
@@ -1,7 +1,6 @@
 package ru.podlubnyy.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,33 +13,13 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname,
-                            Model model) {
-
-//        System.out.println("Hello " + name + " " + surname);
-        model.addAttribute("message","Hello " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("Hello " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
-    }
-
-    @GetMapping("/calculator")
-    public String answer(@RequestParam(value = "a") int a,
-                         @RequestParam(value = "b") int b,
-                         @RequestParam(value = "action") String action,
-                         Model model) {
-        if (action.equals("multiplication")) {
-            model.addAttribute("message","Answer: " + a + " * " + b + " = " + (a * b));
-        } else if (action.equals("addition")) {
-            model.addAttribute("message","Answer: " + a + " + " + b + " = " + (a + b));
-        } else if (action.equals("subtraction")) {
-            model.addAttribute("message","Answer: " + a + " - " + b + " = " + (a - b));
-        } else if (action.equals("division")) {
-            model.addAttribute("message","Answer: " + a + " / " + b + " = " + (a / b));
-        }
-        return "/first/calculator";
     }
 }
